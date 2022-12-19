eec19e315bcb88d439c8dfff7de44ad69356d27c
Cameron J <cjense36@msudenver.edu>
2022-12-18 22:17:31 -0700

    Added some external files to ensure that it works when loaded into IntelliJ IDEA or with sbt. Refactored much of the application.

---
+++ src/main/scala/Board.scala
@@ -0,0 +1,136 @@
+package dev.cjens
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.io.{BufferedSource, Source}
+import scala.util.matching.Regex
+
+class Board {
+  val size: Int = 9
+  val indices: Range = 0 until this.size
+  val boxIndices: Range = 0 until math.sqrt(this.size).toInt
+  var rows: ArrayBuffer[ArrayBuffer[Int]] = ArrayBuffer()
+
+  def fromExisting(b: Board): Board = {
+    this.rows = b.rows
+    this
+  }
+
+  def fromFile(fPath: String): Board = {
+    val fBufSource: BufferedSource = Source.fromFile(fPath)
+    val fIter: Iterator[String] = fBufSource.getLines()
+    val fLines: ArrayBuffer[String] = ArrayBuffer()
+    fIter.foreach(nextLine => fLines += nextLine)
+    val validationPattern: Regex = ("[0-9]" + s"{${this.size}}").r
+    for (line <- fLines) {
+      line match {
+        case validationPattern(_*) => this.rows += ArrayBuffer.from(line.toCharArray.map(_.toString.toInt))
+        case _ => throw new IllegalArgumentException("Error reading board: invalid format")
+      }
+    }
+    this
+  }
+
+  def setElementAt(row: Int, col: Int, value: Int) = {
+    rows(row).update(col, value)
+  }
+
+  def getElements(): IndexedSeq[Int] = {
+    this.getFlatIndexedSequence()
+  }
+
+  def getRow(i: Int): IndexedSeq[Int] = {
+    this.rows(i).toIndexedSeq
+  }
+
+  def getRows(): IndexedSeq[IndexedSeq[Int]] = {
+    val arrayBuffer: ArrayBuffer[IndexedSeq[Int]] = ArrayBuffer()
+    for (i <- this.indices) arrayBuffer += getRow(i)
+    arrayBuffer.toIndexedSeq
+  }
+
+  def getColumn(j: Int): IndexedSeq[Int] = {
+    val transposed = this.rows.transpose
+    transposed(j).toIndexedSeq
+  }
+
+  def getColumns(): IndexedSeq[IndexedSeq[Int]] = {
+    val arrayBuffer: ArrayBuffer[IndexedSeq[Int]] = ArrayBuffer()
+    for (i <- this.indices) arrayBuffer += getColumn(i)
+    arrayBuffer.toIndexedSeq
+  }
+
+  def getBox(x: Int, y: Int): IndexedSeq[Int] = {
+    val boxLen = math.sqrt(this.size).toInt
+    val xIndices = (0 to 8).filter(i =>
+      i >= (x / boxLen) * boxLen && i < (x / boxLen) * boxLen + boxLen)
+    val yIndices = (0 to 8).filter(j =>
+      j >= (y / boxLen) * boxLen && j < (y / boxLen) * boxLen + boxLen)
+    val box: ArrayBuffer[Int] = ArrayBuffer()
+    for {
+      i <- xIndices
+      j <- yIndices
+    } {
+      box.append(this.rows(i)(j).intValue)
+    }
+    box.toIndexedSeq
+  }
+
+  /** Returns an indexed sequence of all boxes in the board. */
+  def getBoxes(): IndexedSeq[IndexedSeq[Int]] = {
+    val arrayBuffer: ArrayBuffer[IndexedSeq[Int]] = ArrayBuffer()
+    for {
+      x <- this.boxIndices
+      y <- this.boxIndices
+    }
+      arrayBuffer += getBox(x, y)
+    arrayBuffer.toIndexedSeq
+  }
+
+  def setBoard(rows: ArrayBuffer[ArrayBuffer[Int]]): Boolean = {
+    var didSucceed: Boolean = false
+    if (rows == null) {
+      this.rows = rows
+      didSucceed = true
+    }
+    didSucceed
+  }
+
+  def setColumn(column_index: Int, column: ArrayBuffer[Int]): Boolean = {
+    var didSucceed: Boolean = false
+    val cols = this.rows.transpose
+    if (column_index < this.size) {
+      cols(column_index) = column
+      this.rows = cols.transpose
+      didSucceed = true
+    }
+    didSucceed
+  }
+
+  def setRow(row_index: Int, row: ArrayBuffer[Int]): Boolean = {
+    var didSucceed: Boolean = false
+    if (row_index < this.size) {
+      this.rows(row_index) = row
+      didSucceed = true
+    }
+    didSucceed
+  }
+
+  override def toString(): String = {
+    val sb = new mutable.StringBuilder()
+    if (this.rows == null) {
+      sb.append("null")
+    }
+    else {
+      for (row <- this.rows) {
+        row.foreach(elem => sb.append(s"$elem"))
+        sb.append("\n")
+      }
+    }
+    sb.toString
+  }
+
+  private def getFlatIndexedSequence(): IndexedSeq[Int] = {
+    this.rows.flatten.toIndexedSeq
+  }
+}
--- src/main/scala/Sudoku.scala
+++ src/main/scala/Sudoku.scala
@@ -5,100 +5,36 @@
  * Description: Prg 02 - Sudoku Puzzle
  */
 
-import scala.collection.immutable.Nil.flatten
+import scala.collection.immutable.Nil.{flatten, iterator}
 import scala.collection.{IterableFactory, mutable}
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.io._
-
 import scala.collection.parallel.ParSeq
 import scala.collection.parallel.CollectionConverters._
-import scala.util.chaining.scalaUtilChainingOps
+import dev.cjens.Board
 
 object Sudoku {
 
   /** Returns a 2D array of Int representing a sudoku board given a filename. */
-  def readBoard(fileName: String, boardSize: Int = 9): Array[Array[Int]] = {
-    val bufferedSource = Source.fromFile(fileName)
-    val boardMtx: Array[Array[Int]] = Array.ofDim(boardSize, boardSize)
-    val source = bufferedSource.getLines.mkString.filter(!_.equals(' '))
-    val validationPattern = ("[0-9]" + s"{${math.pow(boardSize, 2).toInt}}").r
-    source match {
-      case validationPattern(_*) =>
-        for {
-          i <- 0 until boardSize
-          j <- 0 until boardSize
-        } boardMtx(i)(j) = source(boardSize * i + j).asDigit
-      case _ => throw new IllegalArgumentException("Error reading board: invalid format")
-    }
-    boardMtx
-  }
-
-  /** Returns a String representation of a given sudoku board */
-  def boardToString(board: Array[Array[Int]], addWhitespace: Boolean = false): String = {
-    if (board == null) return "Null"
-    val sb = new mutable.StringBuilder()
-    for (x <- board) {
-      if (addWhitespace) x.foreach(y => sb.append(s"$y\t"))
-      else x.foreach(y => sb.append(s"$y"))
-      sb.append('\n')
-    }
-    sb.toString
-  }
-
-  /** Returns a specific row from a sudoku board as a sequence of numbers. */
-  def getRow(board: Array[Array[Int]], row: Int): Array[Int] = {
-    board(row)
-  }
-
-  /** Return a specific column from a sudoku board as a sequence of numbers. */
-  def getCol(board: Array[Array[Int]], col: Int): Array[Int] = {
-    val transposed = board.transpose
-    transposed(col)
-  }
-
-  /** Returns a specific box from a sudoku board as a sequence of numbers. */
-  def getBox(board: Array[Array[Int]], x: Int, y: Int): Array[Int] = {
-    val boxLen = math.sqrt(board.length).toInt
-    val xBoxIndices = (0 to 8).filter(i =>
-      i >= (x / boxLen) * boxLen && i < (x / boxLen) * boxLen + boxLen)
-    val yBoxIndices = (0 to 8).filter(j =>
-      j >= (y / boxLen) * boxLen && j < (y / boxLen) * boxLen + boxLen)
-    val box: ArrayBuffer[Int] = ArrayBuffer()
-    for {
-      m <- xBoxIndices
-      n <- yBoxIndices
-    }
-      box.append(board(m)(n).intValue)
-    box.toArray
+  def readBoard(fileName: String): Array[Array[Int]] = {
+    val board: Board = new Board()
+    board.fromFile(fileName)
+    board.rows.map(_.toArray).toArray
   }
 
   /** Returns an array containing each box in board's array. */
-  def getAllBoxes(board: Array[Array[Int]]): Array[Array[Int]] = {
-    val dim = math.sqrt(board.length).toInt
-    val boxes: Array[Array[Int]] = Array.ofDim(board.length)
-    for {
-      x <- 0 until dim
-      y <- 0 until dim
-    } boxes(x * dim + y) = getBox(board, x, y)
-    boxes
+  def getAllBoxes(board: Board): IndexedSeq[IndexedSeq[Int]] = {
+    board.getBoxes()
   }
 
-  /** Returns true if the sequence is valid, that is, it contains 9 numbers in [0-9] with optionally repeating zeros. */
-  def isValid(seq: Array[Int]): Boolean = {
-    val patternCheckNumeric = raw"[0-9]{9}".r
-    val matchString = seq.mkString
-    matchString match {
-      case patternCheckNumeric(_*) =>
-        val matchSet = matchString.toSet
-        if (matchSet.size.equals(9)) true
-        else if (matchString.toList.count(_.equals('0')).equals(9 - matchSet.size)) false
-        else true
-      case _ => false
-    }
+  /** Returns true if the sequence is valid, */
+  def isValid(seq: IndexedSeq[Int]): Boolean = {
+    (1 to 9).foreach(v => if (seq.count(_.equals(v)) > 1) return false)
+    true
   }
 
   /** A board is valid if all of its rows, columns, and boxes are also valid. */
-  def isValid(board: Array[Array[Int]]): Boolean = {
+  def isValid(board: Board): Boolean = {
     if (allRowsValid(board) &&
       allColsValid(board) &&
       allBoxesValid(board))
@@ -107,105 +43,91 @@ object Sudoku {
   }
 
   /** Returns true if all rows of the given board are valid sequences. */
-  def allRowsValid(board: Array[Array[Int]]): Boolean = {
-    board.foreach { row => if (!isValid(row)) return false }
+  def allRowsValid(board: Board): Boolean = {
+    for (row <- board.getRows())
+      if (isValid(row))
+        return false
     true
   }
 
   /** Returns true if all columns of the given board are valid sequences. */
-  def allColsValid(board: Array[Array[Int]]): Boolean = {
-    board.indices.foreach { i =>
-      if (!isValid(getCol(board, i))) return false
-    }
+  def allColsValid(board: Board): Boolean = {
+    for (column <- board.getColumns())
+      if (isValid(column))
+        return false
     true
   }
 
   /** Return whether all boxes of the given board are valid sequences. */
-  def allBoxesValid(board: Array[Array[Int]]): Boolean = {
-    for {
-      x <- 0 until math.sqrt(board.length).toInt
-      y <- 0 until math.sqrt(board.length).toInt
-    } {
-      if (!isValid(getBox(board, x, y))) return false
-    }
+  def allBoxesValid(board: Board): Boolean = {
+    for (box <- board.getBoxes())
+      if (isValid(box))
+        return false
     true
   }
 
   /** Returns true if board is complete, that is, it contains no zeros. */
-  def isComplete(board: Array[Array[Int]]): Boolean = {
-    !board.flatten.contains(0)
+  def isComplete(board: Board): Boolean = {
+    !board.getElements().contains(0)
   }
 
   /** True if the board is both complete and valid. */
-  def isSolved(board: Array[Array[Int]]): Boolean = {
-    if (isComplete(board) && isValid(board)) true
-    else false
+  def isSolved(board: Board): Boolean = {
+    if (isComplete(board) && isValid(board))
+      true
+    else
+      false
   }
 
-  /** Return a new board configuration from the given one by setting a digit at a specific (row, col) location. */
-  def getChoice(board: Array[Array[Int]], row: Int, col: Int, d: Int): Array[Array[Int]] = {
-    board(row)(col) = d
-    board
+  def getChoices(board: Board): IndexedSeq[Board] = {
+    val choices = mutable.IndexedBuffer[Board]()
+    val zeroIndices = mutable.IndexedBuffer[(Int, Int)]()
+    val allElems = board.getElements()
+    allElems.zipWithIndex.foreach {
+      elemAndIdx =>
+        val value = elemAndIdx._1
+        val index = elemAndIdx._2
+        if (value.equals(0))
+          zeroIndices.append((
+            math.floorDiv(index, board.size),
+            math.floorMod(index, board.size)))
+    }
+    // Create all possible choices and append them to choices
+    val answers = mutable.IndexedBuffer[Int]()
+    for (i <- zeroIndices.indices) answers.append(-1)
+    // TODO: build & return the completed choices sequence or a subsequence thereof
+    //  that we can solve with recursion or (??)
+    ArrayBuffer(new Board()).toIndexedSeq
   }
 
-  def getChoices(board: Array[Array[Int]]): IndexedSeq[Array[Array[Int]]] = {
-    if (isComplete(board)) {
-      return IndexedSeq(board)
-    }
-    val choices: ArrayBuffer[Array[Array[Int]]] = ArrayBuffer()
-    for (thisRow <- board.indices) {
-      for (thisColumn <- board.indices) {
-        val newBoard: Array[Array[Int]] = board.map(_.clone())
-        if (newBoard(thisRow)(thisColumn) == 0) {
-          val row = getRow(newBoard, thisRow).clone
-          val col = getCol(newBoard, thisColumn).clone
-          val box = getBox(newBoard, thisRow, thisColumn).clone
-          val validPredicate: Int => Boolean = v =>
-            !row.contains(v) && !col.contains(v) && !box.contains(v)
-          val validEntries = (1 to 9).filter(validPredicate)
-          validEntries.foreach(e => choices.append(getChoice(newBoard.map(_.clone()), thisRow, thisColumn, e)))
-        }
-      }
-    }
-    choices.toIndexedSeq
+  /** Returns the Crook's algorithm markup for a given cell (valid values of x on [1, 9]) */
+  def getCellMarkup(board: Board, row: Int, col: Int): IndexedSeq[Int] = {
+    val markup = mutable.IndexedBuffer[Int]()
+    markup.append(0).append(1)
+    markup.toIndexedSeq
   }
 
   /** Return a solution to the puzzle (null if there is no solution). */
-  def solve(board: Array[Array[Int]]): Array[Array[Int]] = {
-    val choices = getChoices(board)
-    if (isComplete(board)) {
-      if (isSolved(board)) {
-        return board
-      }
-      else {
-        println(boardToString(board))
-        return null
-      }
+  def solve(board: Board): Board = {
+    val choices: IndexedSeq[Board] = getChoices(board)
+    val parChoices = choices.par.filter { parChoice =>
+      isSolved(parChoice)
     }
-    else {
-      println(s"Parallel: ${countZeros(board)}")
-      choices.par.foreach {
-        choice => solve(choice)
-      }
-    }
-    null
+    if (parChoices.nonEmpty) parChoices(0)
+    else null
   }
 
-  /** Return a string formatted with all elements of array.
-   * Note: Original in-line code provided by IntelliJ IDEA. */
-  def formatStringFromArray[A](array: Array[A]): String = {
-    array.mkString("[", ", ", "]")
-  }
-
-  def countZeros(array: Array[Array[Int]]): Int = {
-    array.flatten.count(_.equals(0))
+  def countZeros(board: Board): Int = {
+    board.getElements().count(_.equals(0))
   }
 
   def main(args: Array[String]): Unit = {
-    var board: Array[Array[Int]] = Array(Array())
-    if (args.length.equals(1)) board = readBoard(args(0))
-    else board = readBoard("boards/sudoku1.txt")
+    val board: Board = new Board()
+    if (args.length.equals(1))
+      board.fromFile(args(0))
+    else
+      board.fromFile("boards/sudoku1.txt")
     val sol = solve(board)
-    println(boardToString(sol))
+    println(sol)
   }
 }
